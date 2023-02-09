a2816817617caae7544756f20d58205d8a76df38
syedameenabrar01 <102346081+syedameenabrar01@users.noreply.github.com>
2023-02-09 18:33:49 +0530

    first commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserHeaderComponent } from './users/layout/user-header/user-header.component';
+import { UserFooterComponent } from './users/layout/user-footer/user-footer.component';
+import { ValuationComponent } from './users/forms/valuation/valuation.component';
+import { UserHomeComponent } from './users/modules/user-home/user-home.component';
+import { AboutComponent } from './users/modules/about/about.component';
+import { ContactComponent } from './users/modules/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
+    ValuationComponent,
+    UserHomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/users/forms/valuation/valuation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-valuation',
+  templateUrl: './valuation.component.html',
+  styleUrls: ['./valuation.component.css']
+})
+export class ValuationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
